14d507ac67679c1c6bd6fab437779cd744c5a309
sebastian-quiceno <sebastian.quicenol@upb.edu.co>
2025-11-09 10:35:39 -0500

    Correcion de ruta de la carpeta Repositories

--- server/bd/bd/src/main/java/com/bases/bd/Repositories/VentaRepository.java
+++ server/bd/bd/src/main/java/com/bases/bd/Repositories/VentaRepository.java
@@ -1,4 +1,4 @@
-package com.bases.Repositories;
+package com.bases.bd.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
